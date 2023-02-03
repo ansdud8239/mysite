c1511ae5fb1c6aa0b937f9b8aa580f41fef943e6
ansdud8239 <ansdud8239@naver.com>
2023-02-03 13:53:38 +0900

    ModelAttribute Valid

--- mysite03/src/main/java/com/douzone/mysite/controller/BoardController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/BoardController.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -60,31 +61,32 @@ public class BoardController{
 	public String view(@RequestParam(value = "n", required = true, defaultValue = "1") Long no,
 			@RequestParam(value = "h", required = true, defaultValue = "1") int hit,
 			@RequestParam(value = "p", required = true, defaultValue = "1") int pageNum, Model model,
-			HttpServletResponse response, HttpServletRequest request) {
-
-		// 쿠키 읽기
-		Cookie[] cookies = request.getCookies();
-		String viewCookie = "";
-		if (cookies != null && cookies.length > 0) {
-			for (Cookie cookie : cookies) {
-				// System.out.println(cookie);
-				if ((no.toString()).equals(cookie.getName())) {
-					viewCookie = cookie.getValue();
+			HttpServletResponse response, HttpServletRequest request,
+			@CookieValue(value = "visit", required=true,defaultValue="") String value) {
+		
+		//System.out.println("visit : "+value);
+		boolean result = true;
+		String[] visitCookie=null;
+		if(!"".equals(value)) {
+			visitCookie = value.split("/");
+			for(String v:visitCookie) {
+				//System.out.println(v);
+				if(no.toString().equals(v)) {
+					result = false;
+					break;
 				}
-			}
+			}			
 		}
 
-		if (viewCookie.equals("")) {
-			// 해당 글을 조회하지 않은 경우
-			Cookie cookieCnt = new Cookie(no.toString(), "cookie");
+		if("".equals(value) || result) {
+			boardService.addContentsHit(no, hit);
+			Cookie cookieCnt = new Cookie("visit", (value+no+"/"));
 			cookieCnt.setPath(request.getContextPath());
 			// 초단위 : 1일 -> 24*60*60
 			cookieCnt.setMaxAge(24 * 60 * 60);
 			response.addCookie(cookieCnt);
-
-			// 조회수 +1
-			boardService.addContentsHit(no, hit);
 		}
+
 		model.addAttribute("vo", boardService.getContents(no));
 		model.addAttribute("pageNum", pageNum);
 		return "board/view";
