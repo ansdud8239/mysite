8c21de1d195c5c8395759178d064a03691d8fe31
ansdud8239 <ansdud8239@naver.com>
2023-02-07 09:09:34 +0900

    ADMIN page

--- mysite03/src/main/java/com/douzone/mysite/security/AuthInterceptor.java
+++ mysite03/src/main/java/com/douzone/mysite/security/AuthInterceptor.java
@@ -1,6 +1,5 @@
 package com.douzone.mysite.security;
 
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -25,15 +24,17 @@ public class AuthInterceptor implements HandlerInterceptor {
 		// 3. Handler Method의 @Auth 가져오기
 		// auth기 null이면 @Auth가 안붙어있음
 		Auth auth = handlerMethod.getMethodAnnotation(Auth.class);
-		
+
 		// 4. Handler Method에 @Auth가 없으면 Type(class)에 붙어있는지 확인
-		auth = (auth == null )? handlerMethod.getBeanType().getAnnotation(Auth.class):auth;
+		auth = (auth == null) ? handlerMethod.getBeanType().getAnnotation(Auth.class) : auth;
+		// auth=(auth == null )?
+		// handlerMethod.getMethod().getDeclaringClass().getAnnotation(Auth.class):auth;
 
 		// 5. Type이나 Method에 @Auth가 없는경우
-		if (auth == null ) {
+		if (auth == null) {
 			return true;
 		}
-		
+
 		// 6. @Auth가 붙어있기 때문에 인증(Authenfication) 여부 확인
 		HttpSession session = request.getSession();
 		UserVo authUser = (UserVo) session.getAttribute("authUser");
@@ -46,13 +47,21 @@ public class AuthInterceptor implements HandlerInterceptor {
 		// 7. 권한체크(Authorization) 체크를 위해 @Auth의 role가져오기("ADMIN","USER")
 		String role = auth.role();
 		String authUserRole = authUser.getRole();
-		System.out.println(role+":"+authUserRole);
-		if(!role.equals(authUserRole)) {
+
+		// 8. @Auth의 role이 "USER"인 경우 , authUser의 role은 상관없다.
+		if ("USER".equals(role)) {
+			return true;
+		}
+
+		// 9. @Auth의 role이 "ADMIN"인 경우, authUser의 role은 반드시 "ADMIN"이어야 한다
+		if (!"ADMIN".equals(authUserRole)) {
 			response.sendRedirect(request.getContextPath());
 			return false;
 		}
-		
-		// 6. 인증확인
+
+		// 10. 옯은 관리자 권한
+		// @Auth의 role :"ADMIN"
+		// authUser의 role : "ADMIN"
 		return true;
 	}
 }
