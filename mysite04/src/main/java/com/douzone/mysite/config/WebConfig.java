996d48a995d9d8ec5293456423d87cfb113672a3
ansdud8239 <ansdud8239@naver.com>
2023-02-13 15:57:16 +0900

    Java Config Migration #2

--- mysite04/src/main/java/com/douzone/mysite/config/WebConfig.java
+++ mysite04/src/main/java/com/douzone/mysite/config/WebConfig.java
@@ -1,8 +1,47 @@
 package com.douzone.mysite.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.douzone.mysite.config.web.FileuploadConfig;
+import com.douzone.mysite.config.web.MessageConfig;
+import com.douzone.mysite.config.web.MvcConfig;
+import com.douzone.mysite.config.web.SecurityConfig;
+import com.douzone.mysite.event.ApplicationContextEventListener;
+import com.douzone.mysite.interceptor.SiteInterceptor;
 
 @Configuration
-public class WebConfig {
+@EnableAspectJAutoProxy
+@ComponentScan({"com.douzone.mysite.controller"})
+@Import({MvcConfig.class,SecurityConfig.class,MessageConfig.class,FileuploadConfig.class})
+public class WebConfig implements WebMvcConfigurer{
+	
+	// Interceptors
+	@Bean
+	public HandlerInterceptor siteInterceptor() {
+		return new SiteInterceptor();
+	}
 
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(siteInterceptor())
+				.addPathPatterns("/**")
+				.excludePathPatterns("/user/auth")
+				.excludePathPatterns("/user/logout")
+				.excludePathPatterns("/asserts/**" );
+		
+	}
+	// ApplicationContextEventListener
+	@Bean
+	public ApplicationContextEventListener applicationContextEventListener() {
+		return new ApplicationContextEventListener();
+	}
+	
+	
 }
